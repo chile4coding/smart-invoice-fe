b6553da77dd2f8df7c5dada4a3d9de322f1cd171
chile4coding <chileomereji@gmail.com>
2026-06-07 21:08:54 +0100

    Comment out hardcoded API base URL for improved flexibility

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://185.200.244.215:4445/api";
+// const API_BASE_URL = "http://185.200.244.215:4445/api";
+const API_BASE_URL = `/api`
 
 const getAccessToken = () => localStorage.getItem("accessToken");
 
